601e29dd155dbb17adda7f48717a2db641cb0177
AlexanderAstanovskiy <alexastanovskiy@gmail.com>
2025-04-17 07:42:45 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
 import os
 from flask import Flask, request
 from telegram import Bot, Update
-from telegram.ext import Dispatcher, MessageHandler, Filters
-from telegram.ext import CallbackContext
+from telegram.ext import Dispatcher, MessageHandler, Filters, CallbackContext
 
 app = Flask(__name__)
 
@@ -17,13 +16,13 @@ dispatcher.add_handler(MessageHandler(Filters.text & ~Filters.command, echo))
 
 @app.route('/')
 def index():
-    return "Bot is alive"
+    return "Bot is running!", 200
 
 @app.route('/webhook', methods=['POST'])
 def webhook():
     update = Update.de_json(request.get_json(force=True), bot)
     dispatcher.process_update(update)
-    return "OK", 200
+    return 'OK', 200
 
-if __name__ == '__main__':
-    app.run(port=10000)
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=10000)
